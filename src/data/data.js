f3ea4564fe480c9ca443a06fa8ae3f44a481b991
Sheena Xu <sheenaxu05@gmail.com>
2025-01-16 13:22:14 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Sheena Xu",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "sheena.xu@ucalgary.ca",
+        phone: "+4039239948",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
